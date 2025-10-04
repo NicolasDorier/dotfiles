21d9710ee20963dbd9426290e5d159789e0ec903
Nicolas Dorier <nicolas.dorier@gmail.com>
2025-10-04 10:18:31 +0900

    save applications

--- setup.sh
+++ setup.sh
@@ -4,3 +4,6 @@ xdg-settings set default-web-browser brave-browser.desktop
 sudo pacman -Sy keepassxc
 # Then instlal https://addons.mozilla.org/en-US/firefox/addon/keepassxc-browser/
 yay -S localsend-bin --no-confirm
+
+yay -S visual-studio-code-bin --no-confirm
+yay -S nautilus-open-any-terminal --no-confirm
