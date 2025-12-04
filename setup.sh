99a1896c06e7031615eea9b741d88468195e5593
Nicolas Dorier <nicolas.dorier@gmail.com>
2025-12-04 16:32:36 +0900

    Mako config

--- setup.sh
+++ setup.sh
@@ -18,6 +18,7 @@ ln -s $(pwd)/config/hypr ~/.config/hypr
 ln -s $(pwd)/applications ~/.local/share/applications
 ln -s $(pwd)/bin/ ~/.local/bin
 ln -s $(pwd)/config/waybar ~/.config/waybar
+ln -s $(pwd)/config/mako ~/.config/mako
 ln -s $(pwd)/config/ghostty ~/.config/ghostty
 ln -s $(pwd)/config/yazi ~/.config/yazi
 ln -s $(pwd)/config/hypr/apps/jetbrains.conf  ~/.local/share/omarchy/default/hypr/apps/jetbrains.conf
