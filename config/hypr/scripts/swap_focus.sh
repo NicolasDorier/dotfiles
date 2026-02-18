e43aae19eddce1062bcf46729d910e5a48cbd953
Nicolas Dorier <nicolas.dorier@gmail.com>
2026-02-18 16:21:52 +0900

    Refactor swap_focus

--- config/hypr/scripts/swap_focus.sh
+++ config/hypr/scripts/swap_focus.sh
@@ -4,18 +4,26 @@
 OUT="/tmp/outwindow"
 IN="/tmp/inwindow"
 
-# read stored window address
-ADDR="$(cat "$OUT" 2>/dev/null)"
-
-# stop if empty
-[ -z "$ADDR" ] && exit 0
-
-# focus the stored window
-hyprctl dispatch focuswindow "address:$ADDR"
-
-# swap files
-TMP=$(mktemp)
-cp "$OUT" "$TMP" 2>/dev/null
-cp "$IN" "$OUT" 2>/dev/null
-cp "$TMP" "$IN" 2>/dev/null
-rm -f "$TMP"
+case "$1" in
+  focusactivetolast)
+    hyprctl dispatch focuscurrentorlast
+    ;;
+  switch)
+    notify-send "Switch mode on"
+    hyprctl activewindow -j | jq -r ".address" > /tmp/outwindow
+    hyprctl -j activewindow | jq -e '.fullscreen == 1' >/dev/null && hyprctl dispatch fullscreen 1
+    rm -f /tmp/inwindow
+    ;;
+  select)
+    notify-send "Switch mode off"
+    hyprctl activewindow -j | jq -r ".address" > /tmp/inwindow
+    OUT_ADDR="$(cat "$OUT" 2>/dev/null)"
+    IN_ADDR="$(cat "$IN" 2>/dev/null)"
+    [ -n "$OUT_ADDR" ] && hyprctl dispatch focuswindow "address:$OUT_ADDR"
+    [ -n "$IN_ADDR" ] && hyprctl dispatch focuswindow "address:$IN_ADDR"
+    hyprctl dispatch fullscreen 1
+    ;;
+  *)
+    exit 1
+    ;;
+esac
